5e998a01d4c701e2b6449074a4b61cbb7a4d0a3d
antn9x <antn9x@gmail.com>
2025-01-30 22:47:33 +0700

    fix click collider

--- src/scene/Game.tsx
+++ src/scene/Game.tsx
@@ -1,23 +1,29 @@
-import { ButtonComp, ComponentX, LabelComp, SceneComponent, SpriteRender } from 'safex'
+import { Collider, ComponentX, ExtraDataComp, FederatedPointerEvent, LabelComp, NodeComp, pointInPolygon, SceneComponent, SpriteRender, TouchEventRegister } from 'safex'
 import { defaultFont, sf_line } from '../assets'
 import Caro from '../components/Caro'
 
 export class Game extends ComponentX {
   score = 0
+  root: NodeComp
+  carosList: Caro[] = []
 
   start() {
-
+    // this.root.instance.interactive = true
   }
 
-  // onUpdate(dt: number) {}
-  onPress(event: ButtonComp) {
-    console.log('Clicked')
-    // this.uiRef.showDialog(true)
+  onTouchStart(event: FederatedPointerEvent) {
+    console.log('Touch start', event, this.carosList.length)
+    const clicked = this.carosList.find(caro => {
+      const points = caro.getComponent(Collider)._worldPoints
+      return pointInPolygon(event.global, points)
+    })
+    console.log('Clicked', clicked?.node.getData('id'))
   }
 
   render() {
     return (
-      <SceneComponent>
+      <SceneComponent $ref={this.root}>
+        <TouchEventRegister onTouchStart={this.onTouchStart.bind(this)} />
         <LabelComp node={{ x: 106, y: 240 }} string="Game" font={defaultFont} />
         {Array(4).map(i => <SpriteRender node={{ y: i * 200 + 300 }} spriteFrame={sf_line} />)}
         {Array(4).map(i => <SpriteRender
@@ -25,7 +31,9 @@ export class Game extends ComponentX {
           spriteFrame={sf_line} />)}
         {Array(3).map((i) =>
           Array(3).map((j) =>
-            <Caro node={{ x: 25 + i * 236, y: 318 + j * 205 }} />)
+            <Caro $push={this.carosList} node={{ x: 25 + i * 236, y: 318 + j * 205 }} >
+              <ExtraDataComp key='id' value={j * 3 + i} />
+            </Caro>)
         )}
       </SceneComponent>
     )
